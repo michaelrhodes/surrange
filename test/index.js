2bffdedc19d64f0f664da5b3849b433d593cf827
michaelrhodes <spam@michaelrhod.es>
2013-10-02 15:45:22 +1000

    Changed tests to use deepEqual instead of joining Arrays for equal.

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ run('Even surround values work nicely', function(test) {
   var expected = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   var result = surrange(15, 10)
   
-  test.equal(result.join(), expected.join())
+  test.deepEqual(result, expected)
   test.end()
 })
 
@@ -13,7 +13,7 @@ run('Negative numbers work as expected', function(test) {
   var expected = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4]
   var result = surrange(-1, 10)
   
-  test.equal(result.join(), expected.join())
+  test.deepEqual(result, expected)
   test.end()
 })
 
@@ -21,7 +21,7 @@ run('Odd surround values get weighted to the right', function(test) {
   var expected = [12, 13, 14, 15, 16, 17, 18, 19]
   var result = surrange(15, 7)
   
-  test.equal(result.join(), expected.join())
+  test.deepEqual(result, expected)
   test.end()
 })
 
@@ -29,7 +29,7 @@ run('Positive flag makes no difference if low bound > 0', function(test) {
   var expected = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   var result = surrange(15, 10, true)
     
-  test.equal(result.join(), expected.join())
+  test.deepEqual(result, expected)
   test.end()
 })
 
@@ -37,12 +37,12 @@ run('Positive flag pushes negative numbers up from 1', function(test) {
   var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
   var result = surrange(-1, 10, true)
 
-  test.equal(result.join(), expected.join(), 'with negative number')
+  test.deepEqual(result, expected, 'with negative number')
  
   expected = [1, 2, 3, 4, 5, 6, 7, 8]
   result = surrange(4, 7, true)
   
-  test.equal(result.join(), expected.join(), 'with negative low bound')
+  test.deepEqual(result, expected, 'with negative low bound')
 
   test.end()
 })
